366403af9240ee8c0189ab437af934b97e949b3e
Muhammed Aly <muhammedaly915@gmail.com>
2022-11-23 22:45:44 +0000

    Update Shelf.js

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import Book from './Book'
+import React from "react";
+import Book from "./Book";
 
+const Shelf = ({ section, books, category, changeShelf }) => {
+  const booksCategory = books.filter((book) => book.shelf === category);
 
-const Shelf = ({section, books, category, changeShelf}) => {
-    const booksCategory = books.filter((book)=> book.shelf === category)
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{section}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {/* Book */}
+          {booksCategory.map((book) => (
+            <Book key={book.id} book={book} changeShelf={changeShelf} />
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-    
-console.log(books)
-
-    return (
-        <div className="bookshelf">
-              <h2 className="bookshelf-title">{section}</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                 {/* Book */}
-                 {booksCategory.map((book)=>(
-
-                 <Book key={book.id}  book={book} changeShelf={changeShelf}/>
-
-                 ))}
-                </ol>
-              </div>
-            </div>  
-    )
-}
-
-export default Shelf
+export default Shelf;
